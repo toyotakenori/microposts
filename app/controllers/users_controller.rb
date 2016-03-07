5080fe492b989b607926af74512ca20c6c31e675
toyotakenori <toyotakenori@gmail.com>
2016-03-07 13:16:27 +0000

    profile modification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+before_action :set_user, only: [:edit, :update]
+
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -17,12 +19,37 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    #セッションのidが編集中ページのidに一致しているか確認
+    if @user.id == session[:user_id]
+      #一致していたら、そのまま処理を進める。
+    else
+      #一致していなかったら、エラーを表示して、プロフィール画面に戻る。
+      flash[:danger] = 'You can only edit yourself.'
+      render 'show'
+    end
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はユーザー画面を表示
+      render 'show' 
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :profile, :area)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :profile, length: { maximum: 510}
+  validates :area, length: { maximum: 300}
   has_secure_password
 end
