9abbd3de521dcd9d62e91f2735aa00cb44a003cc
toyotakenori <toyotakenori@gmail.com>
2016-03-24 10:41:03 +0000

    followings-followers modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+  user = User.create(name:'testuser' , password: '')
+  
+  #rake db:sdeed
