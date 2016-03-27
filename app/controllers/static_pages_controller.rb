7799726ccf65cdcaf68154b7debc3f7e0cb9789c
toyotakenori <toyotakenori@gmail.com>
2016-03-27 08:31:56 +0000

    kaminari

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      @feed_items_kaminari = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page])
     end
   end
 end
